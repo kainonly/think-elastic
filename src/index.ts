66ba3ce7175917943df9234c897588ac7ae65e6c
kain <zhangtqx@vip.qq.com>
2019-07-09 11:58:22 +0800

    Submit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-const {env} = require('process');
-const {library} = require('./library');
-
-
 const main_handler = async (event: any, context: any, callback: any) => {
-    const data: any = await library(env);
-    return data;
+  return {};
 };
 
-export {main_handler};
+export { main_handler };
