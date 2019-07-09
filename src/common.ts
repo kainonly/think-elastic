245089414b3e879b6c311cd3977cd0345d5a72bb
kain <kainpro@foxmail.com>
2019-07-09 23:03:25 +0800

    Submit

--- src/common.ts
+++ src/common.ts
@@ -1,9 +1,13 @@
+import { Bar } from 'cli-progress';
+
 const COS = require('cos-nodejs-sdk-v5');
 const got = require('got');
 const { env } = require('process');
+const progress = require('cli-progress');
 
 const httpClient = got.extend({
   baseUrl: env.SOURCE,
+  retry: 5,
 });
 
 const cos = new COS({
@@ -16,6 +20,10 @@ const params = (args: any) => {
     Bucket: env.BUCKET,
     Region: env.REGION,
   }, args);
+};
+
+const providerPath = (key: string, data: any) => {
+  return '/' + key.replace('%hash%', data.sha256);
 };
 
 const cosGet = (key: string): Promise<any> => new Promise((resolve) => {
@@ -55,4 +63,28 @@ const cosDelete = (key: string): Promise<any> => new Promise((resolve, reject) =
   });
 });
 
-export { httpClient, cosGet, cosPut, cosDelete };
+const setBar = (title: string) => {
+  return new progress.Bar({
+    format: `${title} [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}`,
+    fps: 1,
+  });
+};
+
+const onBar = (bar: Bar, progress: any) => {
+  if (progress.percent === 0) {
+    bar.start(progress.total, 0);
+  } else {
+    bar.update(progress.transferred);
+    if (progress.percent === 1) bar.stop();
+  }
+};
+
+export {
+  httpClient,
+  cosGet,
+  cosPut,
+  cosDelete,
+  providerPath,
+  setBar,
+  onBar,
+};
