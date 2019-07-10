13937e54165bff1b34a7a374ad769fe1f2b41c21
kain <kainpro@foxmail.com>
2019-07-10 18:23:30 +0800

    Submit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { App } from './app';
 
 const main_handler = async (event: any, context: any, callback: any) => {
-  await App();
-  return {};
+  const msg = await App();
+  return {
+    msg,
+  };
 };
 
 export { main_handler };
