62628746a3aa211f61b94685554698e651289b3e
kain <zhangtqx@vip.qq.com>
2019-09-10 09:40:12 +0800

    Submit

--- src/app.ts
+++
@@ -1,5 +0,0 @@
-const App = () => {
-
-};
-
-export { App };
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { App } from './app';
-
-const main_handler = async (event: any, context: any, callback: any) => {
-  return App();
-};
-
-export { main_handler };
