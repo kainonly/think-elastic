9be581a0ff8846c8312e57f47b85e63dc1c0b8bb
kain <zhangtqx@vip.qq.com>
2019-09-11 16:08:01 +0800

    数据异步收集更新

--- src/facade/Elastic.php
+++ src/facade/Elastic.php
@@ -7,7 +7,7 @@ use think\Facade;
 
 /**
  * Class Elastic
- * @method static Client client(string $label = 'default') ElasticSearch 客户端
+ * @method static Client client() ElasticSearch 客户端
  * @method static Client create(string $label) 创建 ElasticSearch 客户端
  * @package think\elastic\facade
  */
